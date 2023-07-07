a80beeef736e489de20b4f453acf410bafb13189
felipebat20 <felipebatista24680@gmail.com>
2023-07-06 22:37:19 -0300

    feat: add SideBar component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Teste"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <SideBar />
+    </div>
+
+    <div class="column is-three-quarter">
+
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+  import { defineComponent } from 'vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+  import SideBar from './components/SideBar.vue';
+
+
+  export default defineComponent({
+    name: 'App',
+    components: {
+      SideBar,
+    }
+  });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>
+      <img
+        src="../assets/logo.png"
+        alt="logo"
+      />
+    </h1>
+  </header>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({ name: 'SideBar' });
+</script>
+
+<style>
+  header {
+    padding: 1rem;
+    background: #0d3b66;
+    width: 100%;
+    height: 100vh;
+  }
+
+  @media only screen and (max-width: 768px) {
+    header {
+      padding: 2.5rem;
+      height: auto;
+    }
+  }
+</style>
