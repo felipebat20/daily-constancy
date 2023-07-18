b841d59543402db835e78be220f96bafd617082d
felipebat20 <felipebatista24680@gmail.com>
2023-07-17 21:55:38 -0300

    feat(Task): show project in task

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,8 +1,12 @@
 <template>
   <Box>
     <div class="columns">
-      <div class="column is-7">
+      <div class="column is-4">
         {{ task.description || 'Unnamed Task' }}
+      </div>
+
+      <div class="column is-3">
+        {{ task.project?.name ?? 'N/D' }}
       </div>
 
       <div class="column">
