a4d1757f8746cb08311a935ec88c898ae4273c89
felipebat20 <felipebatista24680@gmail.com>
2023-10-09 22:53:17 -0300

    make table header fixed

--- src/components/tasks/DCTasksTable.vue
+++ src/components/tasks/DCTasksTable.vue
@@ -5,6 +5,7 @@
     bordered
     title="Treats"
     :rows="tasks"
+    class="my-sticky-header-table"
     :columns="columns"
     row-key="id"
     virtual-scroll
@@ -156,3 +157,31 @@
     // show_modal.value = true;
   };
 </script>
+
+<style lang="sass">
+.my-sticky-header-table
+  /* height or max-height is important */
+  height: 400px
+
+  .q-table__top,
+  .q-table__bottom,
+  thead tr:first-child th
+    /* bg color is important for th; just specify one */
+    background-color: #FFF
+
+  thead tr th
+    position: sticky
+    z-index: 1
+  thead tr:first-child th
+    top: 0
+
+  /* this is when the loading indicator appears */
+  &.q-table--loading thead tr:last-child th
+    /* height of all previous header rows */
+    top: 48px
+
+  /* prevent scrolling behind sticky top row on focus */
+  tbody
+    /* height of all previous header rows */
+    scroll-margin-top: 48px
+</style>
